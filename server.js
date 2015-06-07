f0c0925582739f93f3731c9bea13b2d2ae5a0a98
Aungwara Barteaux <aungwaran@gmail.com>
2015-06-07 11:57:48 -0400

    pages linked

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
 var ejs = require('ejs');
-app.set('view_engine', 'ejs');
+app.set('view engine', 'ejs');
 var request = require('request');
 // var sqlite3 = require("sqlite3").verbose();
 // var db = new sqlite3.Database("puncher.db");
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
-  res.redirect('index.ejs');
+  res.redirect('index.html');
 });
 
 app.get('/game', function(req, res){
